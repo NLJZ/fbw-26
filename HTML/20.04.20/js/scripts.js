8a243ce8b5391523d18353c911dcf138987bdb40
NLJZ <johnson.nathan.lee@gmail.com>
2020-04-20 10:16:09 +0200

    update browser lesson

--- HTML/20.04.20/js/scripts.js
+++ HTML/20.04.20/js/scripts.js
@@ -1,1 +1,23 @@
 console.log("hi from scripts.js");
+
+// window.alert("Hello World");
+// let x = window.prompt("enter your name");
+// console.log(x);
+function change() {
+  window.document.getElementsByTagName("h1")[0].outerHTML =
+    "<h6>Hi, this is me</h6>";
+  let newBox = window.document.getElementById("main");
+  console.log(newBox);
+  newBox.style.backgroundColor = "skyblue";
+  // main.style.backgroundColor = "red";
+}
+
+const multiply = () => {
+  let userNum = document.querySelector("#num").value;
+  // console.log(userNum);
+  let x = "";
+  for (let i = 0; i <= 10; i++) {
+    x += `${i} * ${userNum} = ${i * userNum} <br>`;
+  }
+  document.querySelector(".result").innerHTML = x;
+};
