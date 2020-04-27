494bec2494850e4ccc1682eef669d6bcee6d76eb
NLJZ <johnson.nathan.lee@gmail.com>
2020-04-27 10:14:22 +0200

    update traversing lesson

--- HTML/27.04.20/index.js
+++ HTML/27.04.20/index.js
@@ -7,13 +7,52 @@ let ele2 = document.querySelector("body").previousSibling;
 console.log(ele);
 console.log(ele2);
 
-//firstChild
+// firstChild
 
 let mainText = document.getElementById("text").firstElementChild;
 console.log(mainText);
 console.log(mainText.textContent);
 // nodeName, nodeType, id, innerHTML, innerText
 
+// closest
 let text = document.querySelector("span");
 let closestElement = text.closest(".rad");
 console.log(closestElement);
+
+// matches
+let names = document.querySelectorAll("li");
+names.forEach((name) => {
+  if (name.matches(".teacher")) {
+    console.log(`${name.textContent}`);
+    name.style.fontWeight = "bold";
+  }
+});
+
+let allChildren = document.querySelector("body").children;
+console.log(allChildren);
+
+let namesList = document.querySelector("#names").children;
+console.log(namesList);
+
+// query selector
+let heading = document.querySelector("h1");
+heading.style.backgroundColor = "steelblue";
+// last child element
+
+let menuList = document.getElementById("menu").lastElementChild;
+console.log(menuList);
+
+let allComments = function (element) {
+  let arr = [];
+  for (let i = 0; i < element.childNodes.length; i++) {
+    let node = element.childNodes[i];
+    if (node.nodeType == 8) {
+      arr.push(node);
+    } else {
+      arr.push.apply(arr, allComments(node));
+    }
+  }
+  return arr;
+};
+
+console.log(allComments(document));
