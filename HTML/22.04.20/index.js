200ae2413fc489fc9503f1313cc923d763b1a918
NLJZ <johnson.nathan.lee@gmail.com>
2020-04-28 09:14:33 +0200

    add js from 4.22

--- HTML/22.04.20/index.js
+++ HTML/22.04.20/index.js
@@ -6,3 +6,13 @@ const create = () => {
   newElement.appendChild(newText);
   document.body.appendChild(newElement);
 };
+
+const addToList = () => {
+  let userInput = document.querySelector("#userText").value;
+  console.log(userInput);
+  let newLi = document.createElement("li");
+  let text = document.createTextNode(userInput);
+  newLi.appendChild(text);
+  document.querySelector(".result").appendChild(newLi);
+  document.querySelector("#userText").value = "";
+};
