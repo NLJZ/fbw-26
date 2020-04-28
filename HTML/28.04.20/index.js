31ab3361adb3921e1d20aab23f48e334a76ef3d1
NLJZ <johnson.nathan.lee@gmail.com>
2020-04-28 09:49:40 +0200

    update event listener lesson

--- HTML/28.04.20/index.js
+++ HTML/28.04.20/index.js
@@ -5,10 +5,38 @@
 // Higher order functions aka functions that accept function (callbacks)
 let divText = document.querySelector(".text");
 const print = () => {
-  divText.innerText = "Hi I am onclick from HTML";
+  divText.innerHTML += "Hi I am onclick from HTML<br>";
 };
 
 let btn = document.getElementById("more");
 btn.onclick = function () {
-  divText.innerText = "Hi I am on onclick from javascript file";
+  divText.innerHTML += "Hi I am on onclick from javascript file<br>";
 };
+
+function myFunction() {
+  header.style.backgroundColor = "white";
+}
+//listening to user actions
+// EventTarget.addEventListener(<namespace>, <callback>)
+
+let header = document.querySelector("h1");
+header.addEventListener("mouseover", () => {
+  header.style.backgroundColor = "pink";
+});
+header.addEventListener("mouseleave", myFunction);
+
+function keyUpFunction(e) {
+  this.style.backgroundColor = "pink";
+  console.log(e);
+  if (e.code == "Enter") {
+    print();
+  }
+}
+
+// function keyDownFunction(e) {
+//   if
+// }
+
+let userInput = document.getElementById("userInput");
+userInput.addEventListener("keyup", keyUpFunction);
+userInput.addEventListener("keydown", keyDownFunction);
