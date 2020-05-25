e5d22d4e20b377e06be5ca5b2b2c316a1cf866b6
NLJZ <johnson.nathan.lee@gmail.com>
2020-05-25 10:22:41 +0200

    updating lesson

--- nodejs/25.05.20/index.js
+++ nodejs/25.05.20/index.js
@@ -1,1 +1,26 @@
 // NodeJs
+
+// console.log(process);
+// console.log(process.argv);
+// console.log(`Hey ${process.argv[2]}`);
+
+// process.argv.forEach((item, i) => {
+//   console.log(`${i} : ${item}`);
+// });
+
+const args = process.argv.slice(2);
+console.log(args);
+console.log(`Hey ${args[0]}`);
+
+const fs = require("fs");
+// read file
+fs.readFile("./longText.txt", "utf8", function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+// write file
+fs.writeFile("new.txt", "This is created file in NodeJs", (err) => {
+  if (err) throw err;
+  console.log("Done");
+});
+console.log("Hi this is too fun");
