5002f26295f56c1373a5b545e2d6f15ae037f25b
Nathan Zieser <johnson.nathan.lee@gmail.com>
2020-05-26 09:34:11 +0200

    update node lesson

--- nodejs/26.05.20/index.js
+++ nodejs/26.05.20/index.js
@@ -8,7 +8,26 @@ function parseNum(arg) {
   }
   return number;
 }
+
+function sum(numArray) {
+  return numArray.reduce((sum, num) => sum + num, 0);
+}
+
+function avg(numArray) {
+  return sum(numArray) / numArray.length;
+}
 const [operation, ...rest] = args;
 
 const numbers = rest.map(parseNum);
 // sum 3 5 6 kfjfl
+
+switch (operation) {
+  case "sum":
+    console.log(sum(numbers));
+    break;
+  case "avg":
+    console.log(avg(numbers));
+    break;
+  default:
+    console.log("I cannot do this. please type 'sum' or 'avg' to calculate ");
+}
