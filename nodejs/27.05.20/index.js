cc959b060858ce954c916041161ab935e1dea2bf
NLJZ <johnson.nathan.lee@gmail.com>
2020-05-27 09:11:44 +0200

    add new lesson

---
+++ nodejs/27.05.20/index.js
@@ -0,0 +1,9 @@
+// module exports
+
+const math = require("./math.js");
+let x = 5,
+  y = 4;
+console.log(`x + y = ${math.sum(x, y)}`);
+console.log(`x - y = ${math.sub(x, y)}`);
+console.log(`x * y = ${math.mult(x, y)}`);
+console.log(`x / y = ${math.div(x, y)}`);
