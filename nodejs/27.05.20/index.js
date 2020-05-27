1bd263f2fd20867956059d095f5b7a5936395d10
NLJZ <johnson.nathan.lee@gmail.com>
2020-05-27 09:41:49 +0200

    update lesson

--- nodejs/27.05.20/index.js
+++ nodejs/27.05.20/index.js
@@ -1,5 +1,5 @@
 // module exports
-
+const fs = require("fs");
 const math = require("./math.js");
 let x = 5,
   y = 4;
@@ -7,3 +7,17 @@ console.log(`x + y = ${math.sum(x, y)}`);
 console.log(`x - y = ${math.sub(x, y)}`);
 console.log(`x * y = ${math.mult(x, y)}`);
 console.log(`x / y = ${math.div(x, y)}`);
+
+fs.copyFile("original.txt", "copyFromOg.text", (err) => {
+  if (err) throw err;
+  console.log("Done copying");
+});
+
+const data = fs.readFileSync("math.js");
+console.log(data.toString());
+
+fs.open("new.txt", "r", (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+  console.log("done");
+});
--- nodejs/27.05.20/math.js
+++ nodejs/27.05.20/math.js
@@ -1,4 +1,23 @@
-exports.sum = (a, b) => a + b;
-exports.sub = (a, b) => a - b;
-exports.mult = (a, b) => a * b;
-exports.div = (a, b) => a / b;
+// exports.sum = (a, b) => a + b;
+// exports.sub = (a, b) => a - b;
+// exports.mult = (a, b) => a * b;
+// exports.div = (a, b) => a / b;
+
+// module.exports = {
+//   sum: (a, b) => a + b,
+//   sub: (a, b) => a - b,
+//   mult: (a, b) => a * b,
+//   div: (a, b) => a / b,
+// };
+
+sum = (a, b) => a + b;
+sub = (a, b) => a - b;
+mult = (a, b) => a * b;
+div = (a, b) => a / b;
+
+module.exports = {
+  sum,
+  sub,
+  mult,
+  div,
+};
