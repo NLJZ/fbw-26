f58204f3feb025faef52e3c18a8b244b5d52314e
NLJZ <johnson.nathan.lee@gmail.com>
2020-06-10 09:02:57 +0200

    update lessons

--- nodejs/02.06.20/src/lib/weather.js
+++ nodejs/02.06.20/src/lib/weather.js
@@ -2,16 +2,16 @@ const OpenWeatherClient = require("./weather-client");
 const myKey = require("../../config");
 const API_KEY = myKey;
 
-function formatData (data) {
-	const result = `It is now ${data.main.temp}\u00B0C in ${data.name}, ${data.sys.country}`;
-	const moreInfo = ` Today's weather ${data.weather
-		.map((condition) => condition.description)
-		.join(', ')}`;
-	return result + "\n" + moreInfo;
+function formatData(data) {
+  const result = `It is now ${data.main.temp}\u00B0C in ${data.name}, ${data.sys.country}`;
+  const moreInfo = ` Today's weather ${data.weather
+    .map((condition) => condition.description)
+    .join(", ")}`;
+  return result + "\n" + moreInfo;
 }
 
-module.exports = async function weather (city, country) {
-	const client = new OpenWeatherClient(API_KEY);
-	const currentData = await client.getWeather(city, country);
-	return formatData(currentData);
-}
+module.exports = async function weather(city, country) {
+  const client = new OpenWeatherClient(API_KEY);
+  const currentData = await client.getWeather(city, country);
+  return formatData(currentData);
+};
---
+++ reactjs/09.06.20/first-app/src/js/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello, I am React app</p>
+      <form>
+        <input type="text" placeholder="write sumpthin'" />
+      </form>
+      <img src="./img/elenaonbeachholga.jpg" alt="Elena" />
+    </div>
+  );
+}
+
+export default App;
