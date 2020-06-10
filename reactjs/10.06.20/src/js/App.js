5bf459e558270074bb7fd9c84389f7b40227e999
NLJZ <johnson.nathan.lee@gmail.com>
2020-06-10 09:58:33 +0200

    update lessons

---
+++ reactjs/10.06.20/src/js/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Text from "./text";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hi, I am React.</h1>
+      <Text />
+      <Text />
+      <Text />
+      <Text />
+      <Text />
+    </div>
+  );
+};
+
+export default App;
---
+++ reactjs/10.06.20/src/js/text.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Text = () => {
+  return (
+    <div>
+      <p>
+        Text text text text text text text text text text text text text text
+        text text text text text text text text.
+      </p>
+    </div>
+  );
+};
+
+export default Text;
