69eaf9c763708087848b354c12dcae3029355535
NLJZ <johnson.nathan.lee@gmail.com>
2020-08-14 09:05:50 +0200

    update lessons

---
+++ Backend/12.08.20/index.js
@@ -0,0 +1,10 @@
+// Structured Query Language - SQL
+// ORACLE, MySQL
+
+// SELECT, INSERT, DELETE, UPDATE
+// FROM
+// SELECT id, name, age FROM tableName
+//
+
+// Not only SQL aka NoSql
+// Humongous aka mongoDB
