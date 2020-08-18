c87ee2029f4feb2e6bcf6567f126ddb62ba1758f
NLJZ <johnson.nathan.lee@gmail.com>
2020-08-18 09:07:23 +0200

    update backend lessons

--- Backend/17.08.20/router/employees.js
+++ Backend/17.08.20/router/employees.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-
 const EmployeesData = require("../module/employeesModule");
-// url http://localhost:3000/employees
+// Get all employees
+// url http://localhost:3000/employees/
 router.get("/", async (req, res) => {
   try {
     const employees = await EmployeesData.find();
@@ -11,8 +11,8 @@ router.get("/", async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
-// add employee
-// url http://localhost:3000/employees
+// Add employee
+// url http://localhost:3000/employees/
 router.post("/", async (req, res) => {
   const employee = new EmployeesData({
     name: req.body.name,
@@ -28,5 +28,54 @@ router.post("/", async (req, res) => {
     });
   }
 });
+// GET  http://localhost:3000/employees/ -->  get all employees
+// POST http://localhost:3000/employees/ -->  add employee
+// GET http://localhost:3000/employees/:name -->  get employee by name
+// PATCH http://localhost:3000/employees/:name -->  update employee by name
+// UPDATE aka PUT http://localhost:3000/employees/:name -->  update employee by name
+// DELETE http://localhost:3000/employees/:name -->  delete employee by name
+
+// middleware
+async function getEmployee(req, res, next) {
+  let employee;
+  try {
+    //employee = await EmployeesData.findById(req.params.id);
+    // employee = await EmployeesData.find({ name: req.params.name });
+    employee = await EmployeesData.findOne({ name: req.params.name });
+    if (employee == null)
+      return res.status(404).json({ message: "employee NOT Found" });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+  console.log(employee);
+  //res.employee = employee[0];
+  res.employee = employee;
+  next();
+}
+
+// Get one employee
+// url http://localhost:3000/employees/Ali
+// router.get("/:id", getEmployee, (req, res) => {
+router.get("/:name", getEmployee, (req, res) => {
+  res.status(200).json(res.employee);
+});
+// Update one
+router.patch("/:name", getEmployee, (req, res) => {});
+// Delete
+router.delete("/:name", getEmployee, async (req, res) => {
+  try {
+    await res.employee.remove();
+    res.status(200).json({ message: "Employee Deleted" });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+});
+
+// router.route("/").get(getAllEmployees).post(addNewEmployee);
+// router.route("/:name").get(getOneEmployees).patch(updateOneEmployee);
 
 module.exports = router;
